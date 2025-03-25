60ec6efd5c1af870e9e29ede598b72dab07c1bfc
Victor-ro-dev <victor.ro.dev10@gmail.com>
2025-03-25 13:30:21 -0300

    build: add archi. to schemas validation

---
+++ backend/src/models/schemas/bd_schema.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class SensorTempSchema(BaseModel):
+    id: Optional[int] = None
+    name: str
+    location: str
+    nursing_home_id: Optional[int] = None
+
+    class Config:
+        from_attributes = True
+
+class NursingHomeSchema(BaseModel):
+    id: Optional[int] = None
+    name: str
+    address: str
+
+    class Config:
+        from_attributes = True
+
+class UserSchema(BaseModel):
+    id: Optional[int] = None
+    username: str
+    email: EmailStr
+    password_hash: str
+    nursing_home_id: Optional[int] = None
+
+    class Config:
+        from_attributes = True
+
+class UserLoginSchema(BaseModel):
+    id : Optional[int] = None
+    email: EmailStr
+    password: str
+
+    class Config:
+        from_attributes = True
+
+class UserCreateSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    password_confirmation: str  
+    nursing_home_id: Optional[int] = None
