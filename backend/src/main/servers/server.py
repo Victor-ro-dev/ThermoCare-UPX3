1dd482bd4a4494dae109554d0d5ff74ecaf30af7
Victor-ro-dev <victor.ro.dev10@gmail.com>
2025-03-25 13:37:16 -0300

    build: add server

---
+++ backend/src/main/servers/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from src.main.routes.sensor_route import router as sensor_temp_router
+from fastapi.middleware.cors import CORSMiddleware
+from src.main.routes.nursing_home_route import router as nursing_home_router
+from src.main.routes.user_route import router as user_router
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Incluir as rotas do sensor_temp_router
+app.include_router(sensor_temp_router, prefix="/api/v1")
+app.include_router(nursing_home_router, prefix="/api/v1")
+app.include_router(user_router, prefix="/api/v1")
