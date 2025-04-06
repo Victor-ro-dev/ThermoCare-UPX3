5063c61493b56db58c7a9bb40a732234b988c53b
Victor-ro-dev <victor.ro.dev10@gmail.com>
2025-04-06 19:53:06 -0300

    config: more libs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import AppRoutes from "./routes/AppRoutes"; // Importa o AuthProvider
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "nprogress/nprogress.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
