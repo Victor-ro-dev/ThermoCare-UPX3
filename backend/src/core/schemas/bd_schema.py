f55b527d4cb4a38af287a9a7c65b24c1a94d3a5a
Victor-ro-dev <victor.ro.dev10@gmail.com>
2025-04-06 19:48:06 -0300

    config: att schemas archives

---
+++ backend/src/core/schemas/bd_schema.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class SensorTempSchema(BaseModel):
+    id: Optional[int] = None
+    name: str
+    location: str
+    nursing_home_id: Optional[int] = None
+
+    class Config:
+        from_attributes = True
+
+class NursingHomeSchema(BaseModel):
+    id: Optional[int]
+    name: str
+    cep: str
+    logradouro: str
+    numero: str
+    bairro: str
+    cidade: str
+    estado: str
+    complemento: Optional[str]
+
+    class Config:
+        from_attributes = True
+
+class UserSchema(BaseModel):
+    id: Optional[int] = None
+    username: str
+    email: EmailStr
+    password_hash: str
+    nursing_home_id: Optional[int] = None
+
+    class Config:
+        from_attributes = True
+
+class UserLoginSchema(BaseModel):
+    id : Optional[int] = None
+    email: EmailStr
+    password: str
+
+    class Config:
+        from_attributes = True
+
+class UserCreateSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    password_confirmation: str  
+    nursing_home_id: Optional[int] = None
